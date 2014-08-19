4eef4958a5bdd9289c51a4f1a1aa3f23fd01ddb1
Insane <luojieyy@gmail.com>
2014-08-19 14:43:47 +0800

    change branch for simple example

--- app/plugins/todo/index.js
+++ app/plugins/todo/index.js
@@ -7,49 +7,8 @@ define('Todo', function(){
     icon : 'icon-list',
     init : function(){
       // a basic method for foundry to init your plugin
-      // we will setup a model here
-      var self = this;
-      foundry.model('Todo', ['title','completed'], function(model){
-        // this callback will return the model being created
-        // then you need to make this call to tell foundery 
-        // the current plugin is finished loading and ready
-        foundry.initialized(self.name);
-      });
-    },
-    inited : function(){ 
-       // inited is an optinal method
-       // it will be called when all other plugin is loaded
-       define_controller();
+      
+      foundry.initialized(this.name);
     }
   } 
 });
-
-// maybe some code for angular controller
-function define_controller(){
-  angular.module('foundry').controller('TodoController', ['$scope', function($scope){
-    $scope.todos = [];
-    // get a reference with the model we registered above
-    todo_model = foundry._models.Todo
-
-    $scope.load = function(){
-        $scope.todos = todo_model.all()
-    }
-
-    $scope.add_todo = function(){
-        todo_model.create({title:$scope.todo_title,completed:false});
-
-        $scope.load();
-        $scope.todo_title = '';
-    }
-
-    $scope.delete_todo = function(index){
-        var id = $scope.todos[index].id,
-            todo =todo_model.findByAttribute('id', id);
-
-        todo.destroy();
-        $scope.load();
-    }
-
-    $scope.load();
-  }]);
-}
